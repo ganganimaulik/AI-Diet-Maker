c88e22c867f2a65e01235898c3b8d9b9fa94d9b0
Maulik <ganganimaulik@gmail.com>
2026-06-05 07:34:29 +0530

    fix: remove outdated webVersionCache option from Client configuration

--- whatsapp-worker.js
+++ whatsapp-worker.js
@@ -103,10 +103,6 @@ mongoose.connect(MONGODB_URI, { bufferCommands: false }).then(async () => {
         '--disable-blink-features=AutomationControlled'
       ],
       userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
-    },
-    webVersionCache: {
-      type: 'remote',
-      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
     }
   });
 
